d344d959783b62d80c10c0fd52fa90b04e012e51
Maxim Bondar <jumperishe@gmail.com>
2018-11-13 23:36:05 +0200

    BMP280 sensor add

--- MQTT_Temp.ino
+++ MQTT_Temp.ino
@@ -1,3 +1,10 @@
+
+
+#include <OneWire.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
 #include <WiFiUdp.h>
 #include <WiFiServer.h>
 #include <WiFiClientSecure.h>
@@ -9,38 +16,63 @@
 #include <ESP8266WiFiGeneric.h>
 #include <ESP8266WiFiAP.h>
 #include <ESP8266WiFi.h>
-#include <Adafruit_BMP280.h>
 #include <PubSubClient.h>
 
-const char* ssid = "Edimax_2g";
-const char* password = "magnaTpwd0713key";
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+float t, h, p;
+const char* ssid = "JumperOK";
+const char* password = "specialized200926";
 const char* mqttServer = "109.86.162.193";
 const int mqttPort = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);	
-
+Adafruit_BME280 bme;
+Adafruit_BMP280 bmp;
 void setup()
 {
+	Wire.begin();
+	Wire.pins(D2, D1);
 	Serial.begin(9600);
-	WiFi.begin(ssid, password);
 	
+	Serial.println("Testing BME280 sensor");
+
+	if (bme.begin()) {
+		Serial.println("Could not find BME280 sensor");
+		while (1);
+	}
+	Serial.println("BMP280 OK");
+	
+	Serial.println("Testing BMP280 sensor");
+	if (bmp.begin(0x77)) {
+		Serial.println("Could not find BMP280 sensor");
+		while (1);
+	}
+	Serial.println("BMP280 OK");
+	
+		WiFi.begin(ssid, password);
+	Serial.print("-=Connecting to WiFi=-");
+
 	while (WiFi.status() != WL_CONNECTED) {
-		Serial.print("Connecting to WiFi...");
-		delay(200);
+		Serial.print(".");
+		delay(500);
 	}
 
-	Serial.print("WiFi connected sucesesfuly");
-	Serial.println(WL_IDLE_STATUS);
-						
+	Serial.println("WiFi connected sucesesfuly");
+	delay(1000);
 	client.setServer(mqttServer, mqttPort);
 	//client.setCallback(callback);
 
 	while (!client.connected()) {
-		Serial.println("Connected to MQTT");
+		Serial.println("Connecting to MQTT...");
 
 		if (client.connect("ESP8266Client")) {
-			Serial.println("Connected");
+			Serial.println("Connected to MQTT server 109.86.162.193:1883");
 		}
 	}
 	
@@ -48,9 +80,18 @@ void setup()
 }
 
 void loop()
-{
+{		
+	t = bme.readTemperature();
+	Serial.println(bmp.readTemperature());
 	client.loop();
-	client.publish("esp/test", "hello from esp8266");
-
-	delay(500);
+	client.publish("esp/sensors/temperature","t");
+	
+	
+	client.publish("esp/sensors/humidity", "h");
+	Serial.println(bme.readHumidity());
+	
+	//p = bme.readPressure();
+	client.publish("esp/sensors/pressure", "p");
+	Serial.println(bmp.readPressure());
+	delay(5000);
 }
