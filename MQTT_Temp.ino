d5a8f6c679ac3fbf17b6c8c4f9a3e411d5810784
Maxim Bondar <jumperishe@gmail.com>
2018-11-17 23:52:33 +0200

    Out JSON format of data add...

--- MQTT_Temp.ino
+++ MQTT_Temp.ino
@@ -2,28 +2,20 @@
 
 #include <SparkFunBME280.h>
 #include <Wire.h>
-
-
 #include <WiFiUdp.h>
 #include <WiFiServer.h>
-#include <WiFiClientSecure.h>
 #include <WiFiClient.h>
-#include <ESP8266WiFiType.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiMulti.h>
 #include <ESP8266WiFiGeneric.h>
-//#include <ESP8266WiFiAP.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
 
 
-
+unsigned long timer; //for time delay
 char res[8]; //bufer for conver float to string
 float temper, hum, press;
 const char* ssid = "JumperOK";
-const char* password = "****************";
+const char* password = "*****************";
 const char* mqttServer = "109.86.162.193";
 const int mqttPort = 1883;
 
@@ -77,24 +69,40 @@ void setup()
 
 void loop()
 {
+	if (millis() > (timer + 5000)) {
+		timer = millis();
 
-	client.loop();
-	
-	temper = mySensor.readTempC(), 2;
-	//Serial.println(temper);
-	dtostrf(temper, 4, 2, res);
-	client.publish("esp/sensors/temperature", res);
-	
-	hum = mySensor.readFloatHumidity(), 2;
-	//Serial.println(hum);
-	dtostrf(hum, 4, 2, res);
-	client.publish("esp/sensors/humidity", res);
+		client.loop();
 
-	press = mySensor.readFloatPressure(), 0;
-	press = press / 133.322;
-	//Serial.println(press);
-	dtostrf(press, 4, 2, res);
-	client.publish("esp/sensors/pressure", res);
+		temper = mySensor.readTempC(), 1;
+		//Serial.println(temper);
+		dtostrf(temper, 4, 2, res);
+		client.publish("esp/sensors/temperature", res);
+
+		hum = mySensor.readFloatHumidity(), 1;
+		//Serial.println(hum);
+		dtostrf(hum, 4, 2, res);
+		client.publish("esp/sensors/humidity", res);
+
+		press = mySensor.readFloatPressure();
+		press = press / 133.322;
+		//Serial.println(press);
+		dtostrf(press, 4, 1, res);
+		client.publish("esp/sensors/pressure", res);
 	
-	delay(3000);
+		String json = "{\"data\":{" "\"temperature\": \"" + String(temper) + "\"," "\"humidity\": \"" + String(hum) + "\"," "\"pressure\": \"" + String(press) + "\"}" "}";
+		// Convert JSON string to character array
+		char jsonChar[100];
+		json.toCharArray(jsonChar, json.length() + 1);
+
+		// Publish JSON character array to MQTT topic
+		client.publish("esp/sensors/json", jsonChar);
+
+		Serial.println("");
+		Serial.println("JSON data format");
+		Serial.println(json);
+	}
+	
+	
+
 }
